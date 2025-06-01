dd1ff397c6d0b7af2fe0d9e65f03f99704f094f5
Remchik <art6000.7m@gmail.com>
2025-06-01 06:45:05 +0300

    4

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', event => {
     const delay = formData.get('delay'); 
     const state = formData.get('state'); 
 
-    if (!delay) { 
+    if (!delay || !state) { 
         iziToast.error({
             title: '❌ Error',
             message: 'Введіть затримку',
